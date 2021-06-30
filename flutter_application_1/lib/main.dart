00147fe5b4c643e396915a5dc4e0b71a9d527748
pl98754321 <pream5138@gmail.com>
2021-06-30 20:46:02 +0700

    test on press evnet

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -8,6 +8,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        title: "titlePLuem",
+        home: MyHomepage(),
+        theme: ThemeData(primaryColor: Colors.green.shade50));
+  }
+}
 
-  };
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomepageState createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    int aa = 0;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("AppbartitlePluem"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text("column1"),
+              Text(
+                aa.toString(),
+                style: TextStyle(fontSize: 30, color: Colors.black),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          setState(() {
+            aa++;
+          });
+        }));
+  }
 }
